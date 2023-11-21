15189920176c68169c2cc1e5cb50417957a26339
Christian Nielsen <chrnielsen2003@gmail.com>
2023-11-21 13:28:06 +0100

    start

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+require("dotenv").config();
+
+// Enable CORS
+app.use(cors());
+
+// Define the root route
+app.get("/", (req, res) => {
+  res.send("Hello, World! get");
+});
+
+app.post("/api/checkin", (req, res) => {
+  res.send("Hello, World! post");
+});
+
+const PORT = process.env.PORT || 3003;
+// Start the server
+app.listen(PORT, () => {
+  console.log("Server is running on port " + PORT + ".");
+});
